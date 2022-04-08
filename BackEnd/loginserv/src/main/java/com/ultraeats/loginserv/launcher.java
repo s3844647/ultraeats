9729bbcb81b4a0e362783abe0f1cb00263d7784d
unknown <s3844647@student.rmit.edu.au>
2022-04-08 17:37:04 +1000

    Fixed submodule issue, make sure frontend shows properly

--- BackEnd/loginserv/src/main/java/com/ultraeats/loginserv/launcher.java
+++ BackEnd/loginserv/src/main/java/com/ultraeats/loginserv/launcher.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class launcher {
+public class launcher { //NOTE: Runs on port 2000.
 
 	public static void main(String[] args) {
 		SpringApplication.run(launcher.class, args);
